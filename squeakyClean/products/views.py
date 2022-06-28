d58474cf9935d915a258e079f11ce4d62fb1c2db
AlrxDaley <alexander_daley@icloud.com>
2022-06-28 19:00:11 +0100

    added category filters and also sort by filters

--- squeakyClean/products/views.py
+++ squeakyClean/products/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
 from django.db.models.functions import Lower
+from django.core import serializers
 
 from .models import Product, Category
 
@@ -11,11 +12,15 @@ def all_products(request):
     """ A view to show all products, including sorting and search queries """
 
     products = Product.objects.all()
-    category = Category.name
+    # category = Category.name
     query = None
     categories = None
+    
     sort = None
     direction = None
+
+    # Printing out the 
+    print(request)
 
     if request.GET:
         if 'sort' in request.GET:
@@ -48,13 +53,20 @@ def all_products(request):
 
     current_sorting = f'{sort}_{direction}'
 
+    # The line below needed to be added as the request object being send in is empty, so none of the code above was
+    # being run and the categories object we being left with the value None. The line below just loads all categories
+    categories = Category.objects.all
+
+    # The context setup below said category rather than categories
     context = {
         'products': products,
         'search_term': query,
         'current_categories': categories,
         'current_sorting': current_sorting,
-        'category': category,
+        'categories': categories,
     }
+
+    print(categories)
 
     return render(request, 'products/shop.html', context)
 
@@ -69,3 +81,13 @@ def product_detail(request, product_id):
     }
 
     return render(request, 'products/product_detail.html', context)
+
+def show(request):
+    
+    name = get_object_or_404(Category.__str__)
+    
+    context = {
+        'name': name
+    }
+    
+    return render(request, 'products/shop.html', context)
