cb84a3890d1d8f4ccad594176646ffd9a236c593
AlrxDaley <alexander_daley@icloud.com>
2022-07-12 09:46:36 +0100

    added accounts app

--- squeakyClean/products/urls.py
+++ squeakyClean/products/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+from bag.views import add_to_bag
 
 urlpatterns = [
     path('', views.all_products, name='products'),
     path('<product_id>', views.product_detail, name='product_detail'),
+     path('add/<item_id>/', add_to_bag, name='add_to_bag'),
 ]
